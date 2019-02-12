5e9481fe0060897513a62293ea96cb0a6fcf09a7
Aniket Patel <aniketupatel1@gmail.com>
2019-02-12 22:29:51 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var chocolateBars = [ 'snickers', 'hundred grand', 'kitkat', 'skittles']
+var Cadburry = 'foo';
+
+function addElementToBeginningOfArray(chocolateBars, Cadburry) {
+  return [Cadburry,...chocolateBars];
+}
+
+function  destructivelyAddElementToBeginningOfArray(chocolateBars, Cadburry) {
+  chocolateBars.unshift(Cadburry);
+  return chocolateBars;
+}
+
+function addElementToEndOfArray(chocolateBars, Cadburry) {
+  return [...chocolateBars,Cadburry];
+}
